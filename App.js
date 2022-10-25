0fe3dd241802b81d9af7b667a2336e97f0bf9797
Aluisio <aluisioalves123@gmail.com>
2022-10-24 22:19:05 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import Voice from '@react-native-voice/voice';
 
 export default function App() {
+  let [started, setStarted] = useState(false);
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    }
+  }, []);
+
+  const startSpeechToText = async () => {
+    await Voice.start("pt-BR");
+    setStarted(true);
+  };
+
+  const stopSpeechToText = async () => {
+    await Voice.stop();
+    setStarted(false);
+  };
+
+  const onSpeechResults = (result) => {
+    setResults(result.value);
+  };
+
+  const onSpeechError = (error) => {
+    console.log(error);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!started ? <Button title='Start Speech to Text' onPress={startSpeechToText} /> : undefined}
+      {started ? <Button title='Stop Speech to Text' onPress={stopSpeechToText} /> : undefined}
+      {results.map((result, index) => <Text key={index}>{result}</Text>)}
       <StatusBar style="auto" />
     </View>
   );
