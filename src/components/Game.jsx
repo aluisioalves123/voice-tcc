386bcd70197ff815c495848f4b87bf7ed2cde009
Aluísio Alves <aluisioalves123@gmail.com>
2022-12-11 14:02:37 -0300

    correções no game para usar a nova interface de voz

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,77 +4,11 @@ import { useFocusEffect } from '@react-navigation/native';
 
 import useStore from '../store'
 
-const Game = ({ navigation }) => {
-  const quiz = [
-    {
-      question: '1- Qual a cor do cavalo branco de napoleão',
-      answer: 'A',
-      alternatives: [
-        {
-          option: 'A',
-          answer: 'Branco'
-        },
-        {
-          option: 'B',
-          answer: 'Preto'
-        },
-        {
-          option: 'C',
-          answer: 'Verde'
-        },
-        {
-          option: 'D',
-          answer: 'Vermelho'
-        }
-      ]
-    },
-    {
-      question: '2- Quantas sílabas tem a palavra amendoim?',
-      answer: 'C',
-      alternatives: [
-        {
-          option: 'A',
-          answer: '2'
-        },
-        {
-          option: 'B',
-          answer: '3'
-        },
-        {
-          option: 'C',
-          answer: '5'
-        },
-        {
-          option: 'D',
-          answer: '4'
-        }
-      ]
-    },
-    {
-      question: "3- Qual o nome do autor do livro 'O iluminado'?",
-      answer: 'B',
-      alternatives: [
-        {
-          option: 'A',
-          answer: 'Albert Einstein'
-        },
-        {
-          option: 'B',
-          answer: 'Stephen King'
-        },
-        {
-          option: 'C',
-          answer: 'Aluísio de Azevedo'
-        },
-        {
-          option: 'D',
-          answer: 'Kurt Cobain'
-        }
-      ]
-    }
-  ]
+import { registerAnswer } from '../api/game'
 
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+const Game = ({ navigation }) => {
+
+  const currentQuestion = useStore(state => state.currentQuestion)
   const [selectedOption, setSelectedOption] = useState(null)
   const [answeredOption, setAnsweredOption] = useState(null)
 
@@ -91,18 +25,7 @@ const Game = ({ navigation }) => {
   }
 
   const answerQuestion = () => {
-    setAnsweredOption(selectedOption)
-
-    stopTimer()
-  }
-
-  const nextQuestion = () => {
-    if (quiz[currentQuestion + 1] != undefined) {
-      setCurrentQuestion(currentQuestion + 1)
-      setSelectedOption(null)
-      setAnsweredOption(null)
-      startTimer()
-    }
+    registerAnswer(selectedOption, roomId)
   }
 
   const changeVoiceInterfaceState = useStore((state) => state.changeVoiceInterfaceState)
@@ -115,24 +38,16 @@ const Game = ({ navigation }) => {
 
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-gray-100'>
-      <Text className='text-lg'>{quiz[currentQuestion].question}</Text>
+      <Text className='text-lg'>{currentQuestion.question}</Text>
       <View className='flex flex-row flex-wrap mt-3 p-2 justify-center'>
-        {quiz[currentQuestion].alternatives.map((alternative, index) => (
+        {currentQuestion.alternatives.map((alternative, index) => (
           <Pressable className='p-2 rounded border mr-2 mb-2 basis-2/5'
-                     key={index} onPress={() => setSelectedOption(alternative.option)}
-                     style={{backgroundColor: optionBgColor(alternative.option)}}>
-            <Text>{alternative.option}) {alternative.answer}</Text>
+                     key={index} onPress={() => setSelectedOption(alternative.position)}
+                     style={{backgroundColor: optionBgColor(alternative.position)}}>
+            <Text>{alternative.position}) {alternative.answer}</Text>
           </Pressable>
         ))}
       </View>
-
-      <Pressable className={`p-2 rounded mt-3 bg-blue-800 ${(answeredOption == null) ? '' : 'hidden'}`} onPress={() => answerQuestion()}>
-        <Text className='text-white'>Responder</Text>
-      </Pressable>
-
-      <Pressable className={`p-2 rounded mt-3 bg-blue-800 ${(answeredOption != null) ? '' : 'hidden'}`} onPress={() => nextQuestion()}>
-        <Text className='text-white'>Próxima pergunta</Text>
-      </Pressable>
     </SafeAreaView>
   );
 }
