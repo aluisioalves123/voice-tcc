d2f5e8f28e437571b899e92e47d41d23caf21e4e
Aluísio Alves <aluisioalves123@gmail.com>
2022-12-11 14:03:47 -0300

    mudando room name para room code

--- src/components/EnterRoom.jsx
+++ src/components/EnterRoom.jsx
@@ -5,8 +5,8 @@ import { useFocusEffect } from '@react-navigation/native';
 import useStore from '../store'
 
 const EnterRoom = ({ navigation }) => {
-  const [tempRoomName, setTempRoomName] = useState(null)
-  const [roomName, changeRoomName] = useStore((state) => [state.roomName, state.changeRoomName])
+  const [temproomCode, setTemproomCode] = useState(null)
+  const [roomCode, changeroomCode] = useStore((state) => [state.roomCode, state.changeroomCode])
   const changeVoiceInterfaceState = useStore((state) => state.changeVoiceInterfaceState)
 
   useFocusEffect(
@@ -18,7 +18,7 @@ const EnterRoom = ({ navigation }) => {
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-gray-100'>
       <Text className='text-lg'>Nome da sala</Text>
-      <TextInput className='p-3 rounded border w-80 mb-2' value={tempRoomName}></TextInput>
+      <TextInput className='p-3 rounded border w-80 mb-2' value={temproomCode}></TextInput>
       <Button title='Confirmar'></Button>
     </SafeAreaView>
   );
