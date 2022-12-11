7ff0efc6b0700efa03d0c6bb16699826f8d15397
Aluísio Alves <aluisioalves123@gmail.com>
2022-12-11 14:02:47 -0300

    correções no lobby para usar a interface de voz

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -7,19 +7,14 @@ import useStore from '../store'
 const Lobby = ({ navigation }) => {
 
   const changeVoiceInterfaceState = useStore((state) => state.changeVoiceInterfaceState)
+  const roomUserCount = useStore((state) => state.roomUserCount)
 
-  const roomName = useStore((state) => state.roomName)
-
-  useFocusEffect(
-    useCallback(()=>{
-      changeVoiceInterfaceState('lobby')
-    }, [])
-  )
+  const roomCode = useStore((state) => state.roomCode)
 
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-gray-100'>
-      <Text className='text-lg'>Sala {roomName}</Text>
-      <Text className='text-xl'>1/4 jogadores</Text>
+      <Text className='text-lg'>Sala {roomCode}</Text>
+      <Text className='text-xl'>{roomUserCount}/4 jogadores</Text>
       <Button title='Iniciar partida'/>
       <Button title='Sair da sala'/>
     </SafeAreaView>
